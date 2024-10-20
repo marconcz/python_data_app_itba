120de5b189f3473f72edf8a14d7ecda5bcba6bf9
marconcz <marconcz@gmail.com>
2024-10-20 19:29:24 -0300

    Agregado codigo que descarga parquet y codigo que sube con ORM a la base de redshift

--- cheapshark.py
+++ cheapshark.py
@@ -23,79 +23,90 @@ def get_cheapshark_deals(page):
     except Exception as e:
         print(f"Error al obtener los datos: {e}")
         return None
+    
+def api_to_parquet(start_date):
+    # Son los valores que me parecieron interesantes para extraer
+    # y modelar. 
+    to_extract = {
+                'ID': 'dealID',                     # ID unico de una oferta
+                'GameID': 'steamAppID',             # ID de un juego, comparte ID con la API de Steam, util para enriquecer
+                'StoreID':'storeID',                # ID de la tienda donde esta la oferta (Nosotros siempre traemos Steam = 1, se podria hardcodear)
+                'Name': 'title',                    # Nombre del juego
+                'Metacritic': 'metacriticScore',    # Puntaje del juego
+                'Price': 'normalPrice',             # Precio regular del juego (U$D)
+                'Sale': 'salePrice',                # Precio en esta oferta (U$D)
+                'OnSale': 'isOnSale',               # Esta en oferta ? 1 = SI , 0 = NO
+                'GameRelease': 'releaseDate',       # Fecha en la que salio al mercado el juego
+                'UpdateTime': 'lastChange',         # Fecha del ultimo cambio del juego
+            }
 
-# Son los valores que me parecieron interesantes para extraer
-# y modelar. 
-to_extract = {
-            'ID': 'dealID',                     # ID unico de una oferta
-            'GameID': 'steamAppID',             # ID de un juego, comparte ID con la API de Steam, util para enriquecer
-            'StoreID':'storeID',                # ID de la tienda donde esta la oferta (Nosotros siempre traemos Steam = 1, se podria hardcodear)
-            'Name': 'title',                    # Nombre del juego
-            'Metacritic': 'metacriticScore',    # Puntaje del juego
-            'Price': 'normalPrice',             # Precio regular del juego (U$D)
-            'Sale': 'salePrice',                # Precio en esta oferta (U$D)
-            'OnSale': 'isOnSale',               # Esta en oferta ? 1 = SI , 0 = NO
-            'GameRelease': 'releaseDate',       # Fecha en la que salio al mercado el juego
-            'UpdateTime': 'lastChange',         # Fecha del ultimo cambio del juego
-        }
+    # Lista para ir acumulando las ofertas a medida que recorro la API
+    deal_list = []
 
-# Lista para ir acumulando las ofertas a medida que recorro la API
-deal_list = []
+    # El dia que recibo como parametro
+    current_date = end_date = datetime.strptime(start_date, '%Y-%m-%d')
 
-# El dia que recibo como parametro
-current_date = end_date = datetime.now()
+    # Le resto un dia, voy a hacer una carga de los datos que hay entre 
+    # el parametro recibido y un dia menos, o una cantidad N de dias
+    yesterday_date = current_date - timedelta(days=1)
 
-# Le resto un dia, voy a hacer una carga de los datos que hay entre 
-# el parametro recibido y un dia menos, o una cantidad N de dias
-yesterday_date = current_date - timedelta(days=1)
+    # La fecha en la API viene en formato UNIX TIME
+    # por lo tanto va a ser mejor pasar mis fechas a ese formato
+    # para hacer menos conversiones que transformando fecha a fecha
+    # recibida por la API.
+    end_date = int(end_date .timestamp())
+    timestamp = int(yesterday_date.timestamp())
 
-# La fecha en la API viene en formato UNIX TIME
-# por lo tanto va a ser mejor pasar mis fechas a ese formato
-# para hacer menos conversiones que transformando fecha a fecha
-# recibida por la API.
-end_date = int(end_date .timestamp())
-timestamp = int(yesterday_date.timestamp())
+    # Arranco leyendo la primer pagina
+    page = 0
 
-# Arranco leyendo la primer pagina
-page = 0
+    # timestamp -> La fecha recibida - 1 dia (N dias en caso de recibir parametro)
+    # end_date -> La fecha recibida como parametro
 
-# timestamp -> La fecha recibida - 1 dia (N dias en caso de recibir parametro)
-# end_date -> La fecha recibida como parametro
+    # end_date en inicio es mas reciente que timestamp
+    # a medida que recorro los datos leidos de la API actualizo el end_date
+    # hasta que en cierto momento los datos leidos de la API son menos novedosos
+    # que mi fecha limite 'timestamp', ahi corto, sino al quedarme sin paginas.
+    while (timestamp <= end_date):
+        data = get_cheapshark_deals(page)
+        if data is not None:
+            for deal in data:
+                # Para cada json que representa una oferta, aplico la funcion get()
+                # para obtener los campos que estan en el array 'to_extact'
+                deal_list.append({key: deal.get(value) for key, value in to_extract.items()})
+            end_date = deal_list[-1].get('UpdateTime') # Al final de cada lectura, actualizo end_date por el dato menos novedoso, osea el ultimo en la pagina leida
+            page += 1 # Paso a la siguiente pagina
+            print('Recorriendo pagina: ' + str(page))
+            time.sleep(1)
+        else:  
+            end_date = 0 # Si fallo, la fecha = 0 -> Como estan en formato UNIX TIME es la fecha minima posible y rompe el WHILE
 
-# end_date en inicio es mas reciente que timestamp
-# a medida que recorro los datos leidos de la API actualizo el end_date
-# hasta que en cierto momento los datos leidos de la API son menos novedosos
-# que mi fecha limite 'timestamp', ahi corto, sino al quedarme sin paginas.
-while (timestamp <= end_date):
-    data = get_cheapshark_deals(page)
-    if data is not None:
-        for deal in data:
-            # Para cada json que representa una oferta, aplico la funcion get()
-            # para obtener los campos que estan en el array 'to_extact'
-            deal_list.append({key: deal.get(value) for key, value in to_extract.items()})
-        end_date = deal_list[-1].get('UpdateTime') # Al final de cada lectura, actualizo end_date por el dato menos novedoso, osea el ultimo en la pagina leida
-        page += 1 # Paso a la siguiente pagina
-        print('Recorriendo pagina: ' + str(page))
-        time.sleep(1)
-    else:  
-        end_date = 0 # Si fallo, la fecha = 0 -> Como estan en formato UNIX TIME es la fecha minima posible y rompe el WHILE
+    # Crear un DataFrame de pandas con las novedades
+    new_data = pd.DataFrame(deal_list)
 
-# Crear un DataFrame de pandas con las novedades
-new_data = pd.DataFrame(deal_list)
+    # Ahora si transformo las fechas en formato YYYY-MM-DD
+    new_data['GameRelease'] = pd.to_datetime(new_data['GameRelease'], unit='s').dt.strftime('%Y-%m-%d')
+    new_data['UpdateTime'] = pd.to_datetime(new_data['UpdateTime'], unit='s').dt.strftime('%Y-%m-%d')
 
-# Ahora si transformo las fechas en formato YYYY-MM-DD
-new_data['GameRelease'] = pd.to_datetime(new_data['GameRelease'], unit='s').dt.strftime('%Y-%m-%d')
-new_data['UpdateTime'] = pd.to_datetime(new_data['UpdateTime'], unit='s').dt.strftime('%Y-%m-%d')
+    # Convertir las columnas a los tipos correctos
+    new_data['GameID'] = pd.to_numeric(new_data['GameID'], errors='coerce').fillna(0).astype(int)
+    new_data['StoreID'] = pd.to_numeric(new_data['StoreID'], errors='coerce').fillna(0).astype(int)
+    new_data['Metacritic'] = pd.to_numeric(new_data['Metacritic'], errors='coerce').fillna(0).astype(float)
+    new_data['Price'] = pd.to_numeric(new_data['Price'], errors='coerce').fillna(0).astype(float)
+    new_data['Sale'] = pd.to_numeric(new_data['Sale'], errors='coerce').fillna(0).astype(float)
+    new_data['OnSale'] = pd.to_numeric(new_data['OnSale'], errors='coerce').fillna(0).astype(int)
 
-# Leo los datos que ya tengo cargados de antes
-latest = pd.read_csv('games_data.csv', header=0, sep = ',')
+    # Leo los datos que ya tengo cargados de antes
+    #latest = pd.read_parquet('games_data.parquet')
 
-# Junto ambos conjuntos de datos, concateno los dataframes y luego borro duplicados
-# conservando los nuevos por ID que actua como la PK de mi tabla
-updated_df = pd.concat([latest , new_data]).drop_duplicates(subset = ['ID'], keep = 'last')
+    # Junto ambos conjuntos de datos, concateno los dataframes y luego borro duplicados
+    # conservando los nuevos por ID que actua como la PK de mi tabla
+    #updated_df = pd.concat([latest , new_data]).drop_duplicates(subset = ['ID'], keep = 'last')
 
-# Ordeno por fechas (no es necesario pero lo hacia para chequear luego en excel si actualizo bien)
-updated_df.sort_values(by = 'UpdateTime', ascending = False)
+    # Ordeno por fechas (no es necesario pero lo hacia para chequear luego en excel si actualizo bien)
+    new_data.sort_values(by = 'UpdateTime', ascending = False)
 
-# Guardar el DataFrame en un archivo CSV
-updated_df.to_csv('games_data.csv', index=False)
+    # Guardar el DataFrame en un archivo CSV
+    new_data.to_csv('games_data.csv', index=False)
+    new_data.to_parquet('games_data.parquet', index=False)
+    
---
+++ redshift_conn.py
@@ -0,0 +1,140 @@
+from sqlalchemy import create_engine, String, Integer, Column, Float, ForeignKey, text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+from urllib.parse import quote_plus
+import pandas as pd
+
+# Parámetros de conexión
+database = 'pda'
+host = 'redshift-pda-cluster.cnuimntownzt.us-east-2.redshift.amazonaws.com'
+port = 5439
+user = '2024_patricio_nahuel_perrone'
+password = 'F6@!Q2^9$Lx'
+
+# Crear el URL de conexión
+connection_string = f"redshift+psycopg2://{user}:{quote_plus(password)}@{host}:{port}/{database}"
+
+# Engine y echo = true para poder ver la salida 
+# del motor de base de datos por la consola, util para debug
+engine = create_engine(connection_string, echo=True)
+
+# Voy a crear 2 clases para utilizar el ORM de sqlalchemy
+Base = declarative_base()
+
+class Games(Base):
+    __tablename__ = 'games'
+    __table_args__ = {'schema': '2024_patricio_nahuel_perrone_schema'}
+    
+    id = Column('GameID', Integer, primary_key=True)
+    name = Column('Name', String(400), nullable=True)
+    metacritic_score = Column('Metacritic', Float, nullable=True)
+    game_realease_date = Column('GameRelease', String(10), nullable=True)
+
+    # Relación con Deal
+    deals = relationship("Deal", back_populates="game")
+
+class Deal(Base):
+    __tablename__ = 'cheapshark_deals'
+    __table_args__ = {'schema': '2024_patricio_nahuel_perrone_schema'}
+    
+    id = Column("ID", String(200), primary_key=True)
+    steam_id = Column('GameID', Integer, ForeignKey('2024_patricio_nahuel_perrone_schema.games.GameID'), nullable=True)
+    store_id = Column('StoreID', Integer, nullable=True)         
+    sale_price = Column('Sale', Float, nullable=True)        
+    is_on_sale = Column('OnSale', Integer, nullable=True)        
+    deal_update_date = Column('UpdateTime', String(10), nullable=True)   
+
+    # Relación inversa
+    game = relationship("Games", back_populates="deals")
+
+# Esto crea las tablas (solamente si no existen previamente)
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+
+# Aca le doy forma a los objetos que voy a insertar
+def prepare_game(row):
+    return Games(
+        id=int(row['GameID']),
+        name=row['Name'],
+        metacritic_score=float(row['Metacritic']) if row['Metacritic'] else None,
+        game_realease_date=row['GameRelease']
+    )
+
+def prepare_deal(row, game_id):
+    return Deal(
+        id=row['ID'],
+        steam_id=game_id,
+        store_id=int(row['StoreID']),  # Asegúrate de convertir a entero
+        sale_price=float(row['Sale']) if row['Sale'] else None,
+        is_on_sale=int(row['OnSale']) if row['OnSale'] else None,
+        deal_update_date=row['UpdateTime']
+    )
+
+try:
+    with Session() as conn:
+        # Seteo el esquema proporcionado por la catedra
+        conn.execute(text(f"SET search_path TO '2024_patricio_nahuel_perrone_schema';"))
+        
+        # Leo el stage
+        df = pd.read_parquet('games_data.parquet')
+        
+        # Filtramos los registros con 'UpdateTime' mayor a la fecha de corte
+        df = df[df['UpdateTime'] > cutoff_date]
+        
+        # Obtener todos los juegos y deals existentes en la base de datos
+        existing_games = {game.id: game for game in conn.query(Games).all()}
+        existing_deals = {deal.id: deal for deal in conn.query(Deal).all()}
+
+        game_listings = []
+        deal_listings = []
+
+        for index, row in df.iterrows():
+            game_id = int(row['GameID'])
+            game = prepare_game(row)
+
+            # Si el juego ya existe en la BBDD, lo actualizo
+            if game_id in existing_games:
+                existing_game = existing_games[game_id]
+                existing_game.name = game.name
+                existing_game.metacritic_score = game.metacritic_score
+                existing_game.game_realease_date = game.game_realease_date
+            else:
+                # Si el juego no existe, lo agrego al lote
+                game_listings.append(game)
+
+            # Preparar el objeto Deal
+            deal = prepare_deal(row, game_id)
+
+            # Si el Deal ya existe, lo actualizo
+            if deal.id in existing_deals:
+                existing_deal = existing_deals[deal.id]
+                existing_deal.steam_id = deal.steam_id
+                existing_deal.store_id = deal.store_id
+                existing_deal.sale_price = deal.sale_price
+                existing_deal.is_on_sale = deal.is_on_sale
+                existing_deal.deal_update_date = deal.deal_update_date
+            else:
+                # Si el Deal no existe, lo agrego al lote
+                deal_listings.append(deal)
+
+            # Hacer commits en lotes de 100 para evitar cargar demasiado la memoria
+            if len(deal_listings) >= 100 or len(game_listings) >= 100:
+                conn.bulk_save_objects(game_listings)
+                conn.bulk_save_objects(deal_listings)
+                deal_listings = [], game_listings = []
+
+        # Inserción de los juegos en bulk
+        if game_listings:
+            conn.bulk_save_objects(game_listings)
+            
+        # Guardar el último lote si quedó sin insertar
+        if deal_listings:
+            conn.bulk_save_objects(deal_listings)
+
+
+        # Commit final para confirmar los cambios
+        conn.commit()     
+
+except Exception as e:
+    print(f"Error al conectar a Redshift: {e}")
